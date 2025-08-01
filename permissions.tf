980218de73312c26aaf6ae291a69f2ee96991068
Vladan Markisic <vladan.markisic@devoteam.com>
2025-08-01 11:28:57 +0200

    New changes to terraform execution. First create placeholder image, then trigger front and back deploy #4

--- permissions.tf
+++
@@ -1,6 +0,0 @@
-# grants the terraform service account permission to trigger cloud builds
-resource "google_project_iam_member" "cloudbuild_trigger_permission" {
-  project = "sara-sandbox-interns"
-  role    = "roles/cloudbuild.editor"
-  member  = "serviceAccount:vladan-iac-terraform-deployer@sara-sandbox-interns.iam.gserviceaccount.com"
-}
